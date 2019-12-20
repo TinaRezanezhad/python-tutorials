baade5af13964c19ccf985175ecb8519a65c2079
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2019-12-20 15:58:37 +0330

    add several input solution

--- several_input/sevinput.py
+++ several_input/sevinput.py
@@ -7,11 +7,10 @@
 number = int(input())
 max_input = 0
 for i in range (number):
-    req_input = input()
+    req_input = int(input())
     if req_input > max_input:
         max_input = req_input
     else:
-        
-        
-    print(max_input)
+        req_input < max_input 
+print(max_input)
 
