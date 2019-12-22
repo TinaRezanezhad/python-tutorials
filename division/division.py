f317d6b4a4e9be2ef2bb43468bcd9ce88e54102a
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2019-12-23 01:28:23 +0330

    division and print int and float

---
+++ division/division.py
@@ -0,0 +1,11 @@
+# Read two integers and print two lines.
+# The first line should contain integer division
+# The second line should contain float division,
+# You don't need to perform any rounding or formatting operations.
+
+a = int(input())
+b = int(input())
+division_int = int(a / b)
+division_float = float(a / b)
+print(division_int)
+print(division_float)
