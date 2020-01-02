23465f4f24c95c575ee39004d23ba34ca14cd3aa
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2020-01-03 01:24:17 +0330

    find secound score

---
+++ runnerupscore/runnerupscore.py
@@ -0,0 +1,9 @@
+# get score from input
+# make a list of scores
+# compaire score with eachothers
+# find secound score from list
+# print secound score
+
+score = int(input())
+scores_list = list(map(int, input().split()))
+
